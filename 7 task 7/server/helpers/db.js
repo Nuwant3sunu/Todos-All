53cf5a379332fd7022ecdc714ade3d062a6841c1
nuwan98765432 <t3sunu00@students.oamk.fi>
2024-03-22 20:31:03 +0200

    task 7 comleted

--- 7 task 7/server/helpers/db.js
+++ 7 task 7/server/helpers/db.js
@@ -2,12 +2,12 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const query = (sql,values = []) => {
-    return new Promise(async (resolve, reject) => {
+    return new Promise(async (resolve,reject) => {
         try {
             const pool = openDb();
             const result = await pool.query(sql,values);
             resolve(result);
-        } catch (error) {
+        }   catch (error) {
             reject(error.message);
         }
     });
--- 7 task 7/server/routes/todo.js
+++ 7 task 7/server/routes/todo.js
@@ -1,25 +1,24 @@
 const express = require('express')
-const {query} = require('../helpers/db.js')
+const { query } = require('../helpers/db.js')
 
 const todoRouter = express.Router()
 
 todoRouter.get("/",async (req,res)=>{
     
     try {
-        const result = await query('SELECT * FROM TASK')
+        const result = await query('select * from task')
         const rows = result.rows ? result.rows : [];
         res.status(200).json(rows);
         
     } catch (error) {
-        console.log(error);
         res.statusMessage = error
-        res.status(500).json({error :error});
+        res.status(500).json({error: error});
     }
 });
 
 todoRouter.post("/new",async (req,res) => {
     try {
-        const result = await query('INSERT INTO task (description) VALUES ($1) RETURNING *',
+        const result = await query('insert into task (description) values ($1) returning *', 
         [req.body.description])
         res.status(200).json({id:result.rows[0].id});
     } catch (error) {
@@ -31,11 +30,11 @@ todoRouter.post("/new",async (req,res) => {
 
 /*Start by implementing deletion functionality to the backend. Create delete method, that
 receives id as query parameter (e.g., http://localhost:3001/delete/1).*/
-todoRouter.delete("/delete/:id", async(req, res) => {
+todoRouter.delete("/delete/:id",async(req,res) => {
     const id = Number(req.params.id);
     try {
-        const result = await query('DELETE FROM task WHERE id = $1',
-        [id]);
+        const result = await query('delete from task where id = $1',
+        [id])
         res.status(200).json({id:id});
     } catch (error) {
         console.log(error);
@@ -43,3 +42,5 @@ todoRouter.delete("/delete/:id", async(req, res) => {
         res.status(500).json({error: error});
     }
 })
+
+module.exports = {todoRouter};
